87f621974280e410655ebae5a38dcdd7a59f23a8
Aarav Jain <ajvid220512@gmail.com>
2026-06-24 18:30:12 +0530

    Update greeting message and prompt for input

--- hello_project/src/main.rs
+++ hello_project/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
 fn main() {
-    println!("Hello,🦀 from Cargo!");
+    println!("Hello,Welcome to guessing Game.");
+
+    println!("Please Enter Your Input:");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("sorry amigo!");
+
+    println!("You Guessed {guess}")
 }
