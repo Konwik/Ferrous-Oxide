dc0c91e9b96c71811f59dfea44ae55d7b0020947
Aarav Jain <ajvid220512@gmail.com>
2026-06-25 08:41:33 +0530

    Removed function from main.rs

--- hello_project/src/main.rs
+++ hello_project/src/main.rs
@@ -1,13 +1,1 @@
-use std::io;
-fn main() {
-    println!("Hello,Welcome to guessing Game.");
 
-    println!("Please Enter Your Input:");
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("sorry amigo!");
-
-    println!("You Guessed {guess}")
-}
